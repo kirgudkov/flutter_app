b05ff3bcc1ffdf4f93374885fb6033c950d579c7
kirillgudkov <for.example.nickname@gmail.com>
2019-05-11 22:42:46 +0300

    fix search

--- lib/Repository.dart
+++ lib/Repository.dart
@@ -7,7 +7,7 @@ class Repository {
   static String url = 'https://itunes.apple.com/search?country=US&entity=movie&term=';
 
   static Future<MovieList> find(String searchString) async {
-    final response  = await http.get('$url/${searchString ?? ' '}');
+    final response  = await http.get('$url/$searchString');
     return MovieList.fromJson(json.decode(response.body));
   }
 }
